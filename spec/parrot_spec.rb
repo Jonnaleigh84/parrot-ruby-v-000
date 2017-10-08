87aac9a5374b612f0144e6e6e2e69410524a0369
Jonna Leigh <zanycandlecreations@gmail.com>
2017-10-08 07:19:01 +0000

    automatically backed up by learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -3,7 +3,7 @@ require_relative '../parrot.rb' # Code your solution in this file
 
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
-    expect($stdout).to receive(:puts).with("Squawk!")
+    expect($stdout).to receive(:puts).with("Prettu Bird!")
 
   end
 
